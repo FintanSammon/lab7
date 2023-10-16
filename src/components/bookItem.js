efbc05f0a05c319869ad0a9602193a502f5aca6a
unknown <g00384403@atu.ie>
2023-10-16 14:40:32 +0000

    first commit

--- src/components/bookItem.js
+++ src/components/bookItem.js
@@ -1,23 +1,23 @@
 //For bookItem it displays 
 import Card from 'react-bootstrap/Card';
-function BookItem(props){
-return(
+function BookItem(props) {
+  return (
     <div>
 
-  
-<Card>
-      <Card.Header>{props.mybook.title}</Card.Header>
-      <Card.Body>
-        <blockquote className="blockquote mb-0">
-          <img src = {props.mybook.thumbnailUrl}></img>
-          <footer>
-            {props.mybook.authors[0]}
-          </footer>
-        </blockquote>
-      </Card.Body>
-    </Card>
+
+      <Card>
+        <Card.Header>{props.mybook.title}</Card.Header>
+        <Card.Body>
+          <blockquote className="blockquote mb-0">
+            <img src={props.mybook.thumbnailUrl}></img>
+            <footer>
+              {props.mybook.authors[0]}
+            </footer>
+          </blockquote>
+        </Card.Body>
+      </Card>
     </div>
-);
+  );
 }
 
 export default BookItem;
--- src/components/create.js
+++ src/components/create.js
@@ -1,6 +1,57 @@
-function Create(){
-    return(
+import { useState } from "react";
+
+function Create() {
+
+    const [title, setTitle] = useState('');
+    const [cover, setCover] = useState('');
+    const [author, setAuthor] = useState('');
+    
+    const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log("Title: "+title+
+    " Cover:" +cover+
+    " Author: " +cover);
+    }
+    
+    return (
+        <div>
             <h2>This is my Create Component</h2>
+            <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <label>Add Book Title: </label>
+                    <input type="text"
+                        className="form-control"
+                        value={title}
+                        onChange={(e) => { setTitle(e.target.value) }}
+                    />
+                </div>
+
+                <div className="form-group">
+                    <label>Add Book Cover: </label>
+                    <input type="text"
+                        className="form-control"
+                        value={cover}
+                        onChange={(e) => { setCover(e.target.value) }}
+                    />
+                </div>
+
+                <div className="form-group">
+                    <label>Add Book Author: </label>
+                    <input type="text"
+                        className="form-control"
+                        value={author}
+                        onChange={(e) => { setAuthor(e.target.value) }}
+                    />
+                </div>
+                <div>
+          <input
+            type="submit"
+            value="Create Book"
+          />
+        </div>
+            </form>
+        </div>
     );
 }
 
