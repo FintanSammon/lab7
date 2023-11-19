367564fde575eefdc4a2b9ddfdeca914726843c0
FintanSammon <72512712+FintanSammon@users.noreply.github.com>
2023-11-19 19:33:07 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// Importing necessary React modules and styles
+import React from 'react'; // Main React library
+import ReactDOM from 'react-dom/client'; // ReactDOM for rendering into the DOM
+import './index.css'; // Custom CSS for styling the application
+import App from './App'; // Main App component
+import reportWebVitals from './reportWebVitals'; // Utility for measuring performance
 
+// Creating a root container where the React app will be mounted
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Rendering the App component inside the root container
 root.render(
   <React.StrictMode>
+    {/* StrictMode is a tool for highlighting potential problems in an application */}
     <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// This section is for performance monitoring
+// reportWebVitals() is a function that can be used to log performance metrics
+// Here, it's called without arguments, so it does nothing by default
+// To enable logging, pass a function like console.log, or set up a custom logging function
+// For more advanced usage, metrics can be sent to an analytics endpoint
+// More information available at: https://bit.ly/CRA-vitals
 reportWebVitals();
