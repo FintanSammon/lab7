4939a503a9dd5215cc3ef13abeeef5e20fc81767
FintanSammon <72512712+FintanSammon@users.noreply.github.com>
2023-11-19 19:25:05 +0000

    Update read.js

--- src/components/read.js
+++ src/components/read.js
@@ -1,34 +1,39 @@
+// Importing necessary hooks and components
 import { useEffect, useState } from "react";
 import Books from "./books";
 import axios from "axios";
 
+// Functional component 'Read' to fetch and display book data
 function Read() {
-
-
+    // State to store the book data
     const [data, setData] = useState([]);
-//use effect is a react hook that allows 
+
+    // useEffect hook to perform side effects in this functional component
     useEffect(() => {
-        //Axious
+        // Using axios to fetch book data from the API
         axios.get('http://localhost:4000/api/books')
             .then(
                 (response) => {
+                    // Updating the state with the data received from the server
                     setData(response.data);
                 }
             )
             .catch(
                 (error) => {
+                    // Logging errors to the console if the request fails
                     console.log(error);
                 }
             );
-    }, []);
+    }, []); // Empty dependency array to run the effect once on component mount
 
     return (
         <div>
             <h2>Hello from my Read Component</h2>
+            {/* Passing the fetched book data to the Books component as props */}
             <Books myBooks={data}></Books>
         </div>
     );
-
 }
 
+// Exporting the Read component for use in other parts of the app
 export default Read;
